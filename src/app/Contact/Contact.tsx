15bbfe8d2031f10afa522ccdfc1100c012ffe2c0
Alexandre <alexandre.hernandez@yahoo.com>
2024-03-16 00:09:14 +0100

    16/03/24

--- src/app/Contact/Contact.tsx
+++ src/app/Contact/Contact.tsx
@@ -9,16 +9,18 @@ export default function Contact() {
         <p className="lg:text-xl text-sm text-textBlancDark-1000">Recherche stage pour valider mon année d&apos;étude</p>
       </div>
       <div className="flex flex-col justify-between gap-2">
-        <button
-          className="text-sm md:text-base relative px-1 py-2 lg:px-10 md:py-3 border-textBlancDark-1000 text-textBlancDark-1000 rounded-md bg-backgroundDark-1000 isolation-auto z-10 border-2 border-gradient
-          before:absolute before:w-full before:transition-all before:duration-500 before:hover:w-full before:-right-full before:hover:right-0 before:rounded-full  before:bg-gradient before:-z-10  before:aspect-square before:hover:scale-150 overflow-hidden before:hover:duration-500 whitespace-nowrap"
-        >
-          Me contacter
-        </button>
-        <button className=" relative lg:px-10 px-1 py-2 md:py-3 text-sm md:text-base text-textBlancDark-1000 rounded-md bg-gradient   ease-in-out  lg:hover:border-accentDark-1000 lg:outline outline-offset-2 outline-1 outline-gradient lg:hover:scale-[1.03] lg:hover:outline-none duration-300 ">
+        <a href="mailto:alexandre.hernandez@yahoo.com">
+          <button 
+            className="text-sm md:text-base relative px-1 py-2 lg:px-10 md:py-3 border-textBlancDark-1000 text-textBlancDark-1000 rounded-md bg-backgroundDark-1000 isolation-auto z-10 border-2 border-gradient
+            before:absolute before:w-full before:transition-all before:duration-500 before:hover:w-full before:-right-full before:hover:right-0 before:rounded-full  before:bg-gradient before:-z-10  before:aspect-square before:hover:scale-150 overflow-hidden before:hover:duration-500 whitespace-nowrap"
+          >
+            Me contacter
+          </button>
+        </a>
+        <a href="https://cvdesignr.com/p/64be66892cb12" className=" relative lg:px-10 px-1 py-2 md:py-3 text-sm md:text-base text-textBlancDark-1000 rounded-md bg-gradient   ease-in-out  lg:hover:border-accentDark-1000 lg:outline outline-offset-2 outline-1 outline-gradient lg:hover:scale-[1.03] lg:hover:outline-none duration-300 ">
           
           Voir mon CV
-        </button>
+        </a>
         
       </div>
     </footer>
